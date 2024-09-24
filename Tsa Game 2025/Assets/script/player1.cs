60e0bb2668c7acdda2689bb30781f5acd41e4b4e
RaceAwesome52 <158531242+RaceAwesome52@users.noreply.github.com>
2024-09-24 16:51:54 -0400

    Added character movement
    it was very epic

--- Tsa Game 2025/Assets/script/player1.cs
+++ Tsa Game 2025/Assets/script/player1.cs
@@ -3,16 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player1 : MonoBehaviour
-{
+{   
+    public Rigidbody2D player1RB;
+    public Transform player1Transforms;
+    public float speed;
+    public float jump;
+    public bool isFacingRight;
+    public bool isTouchingGround;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isFacingRight = true;
+        speed = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //gets the inputs of the 
+        float horizontal= Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        if(Input.GetButton("Horizontal")){
+            player1RB.velocity = new Vector2(horizontal * speed, player1RB.velocity.y);
+        }
+        if(Input.GetButton("Vertical")&&isTouchingGround==true){
+            player1RB.velocity = new Vector2(player1RB.velocity.x, jump);
+        }
+    }
+    public void Flip(){
+        Vector2 currentscale = gameObject.transform.localScale;
+        currentscale.x*=-1;
+        gameObject.transform.localScale= currentscale;
+        isFacingRight= !isFacingRight;
+    }
+    public void OnCollisionStay2D(Collision2D collision){
+        if(collision.gameObject.tag=="ground"){
+            isTouchingGround=true;
+        }
+    }
+    public void OnCollisionExit2D(Collision2D collision){
+        if(collision.gameObject.tag=="ground"){
+            isTouchingGround=false;
+        }
     }
 }
--- Tsa Game 2025/Assets/script/player2.cs
+++ Tsa Game 2025/Assets/script/player2.cs
@@ -3,16 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player2 : MonoBehaviour
-{
+{   
+    public Rigidbody2D player2RB;
+    public Transform player2Transforms;
+    public float speed;
+    public float jump;
+    public bool isFacingRight;
+    public bool isTouchingGround;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isFacingRight = true;
+        speed = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //gets the inputs of the 
+        float horizontal= Input.GetAxisRaw("Horizontal2");
+        float vertical = Input.GetAxisRaw("Vertical");
+        if(Input.GetButton("Horizontal2")){
+            print("huh");
+            player2RB.velocity = new Vector2(horizontal * speed, player2RB.velocity.y);
+        }
+        if(Input.GetButton("Vertical2")&&isTouchingGround==true){
+            player2RB.velocity = new Vector2(player2RB.velocity.x, jump);
+        }
+    }
+    public void Flip(){
+        Vector2 currentscale = gameObject.transform.localScale;
+        currentscale.x*=-1;
+        gameObject.transform.localScale= currentscale;
+        isFacingRight= !isFacingRight;
+    }
+    public void OnCollisionStay2D(Collision2D collision){
+        if(collision.gameObject.tag=="ground"){
+            isTouchingGround=true;
+        }
+    }
+    public void OnCollisionExit2D(Collision2D collision){
+        if(collision.gameObject.tag=="ground"){
+            isTouchingGround=false;
+        }
     }
 }
