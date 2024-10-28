052a5bf15bb20bcd418fd03c2aff9f45da8e4112
RaceAwesome52 <158531242+RaceAwesome52@users.noreply.github.com>
2024-10-28 19:03:10 -0400

    put some sprites in

--- Tsa Game 2025/Assets/script/enemy1movement.cs
+++ Tsa Game 2025/Assets/script/enemy1movement.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class enemy1movement : MonoBehaviour
 {
+    public string direction;
+    public int speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed=3;
+        direction="right";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (direction == "left") {
+            transform.position += speed * Time.deltaTime * -transform.right;
+            Vector2 currentscale = transform.localScale;
+            currentscale.x = 1f;
+            transform.localScale = currentscale;
+        }
+        if (direction == "right")
+        {
+            transform.position += speed * Time.deltaTime * transform.right;
+            Vector2 currentscale = transform.localScale;
+            currentscale.x = -1f;
+            transform.localScale = currentscale;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag=="right movement"){
+            direction="right";
+        }
+        if(other.tag=="left movement"){
+            direction="left";
+        }
     }
 }
