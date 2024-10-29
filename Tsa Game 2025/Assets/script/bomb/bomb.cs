adb9a2522e7f77ac6a9bc2985b5e97f11d2b3c79
RaceAwesome52 <158531242+RaceAwesome52@users.noreply.github.com>
2024-10-29 08:15:47 -0400

    make bomb look a bit better

--- Tsa Game 2025/Assets/script/bomb/bomb.cs
+++ Tsa Game 2025/Assets/script/bomb/bomb.cs
@@ -7,12 +7,15 @@ public class bomb : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("death",2.7f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void death(){
+        Destroy(this.gameObject);
     }
 }
