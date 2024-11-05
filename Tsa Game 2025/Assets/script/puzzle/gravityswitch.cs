9987af1b716599bdda68354f05a774acaea31547
RaceAwesome52 <158531242+RaceAwesome52@users.noreply.github.com>
2024-11-04 21:02:40 -0500

    started on gravity thing

---
+++ Tsa Game 2025/Assets/script/puzzle/gravityswitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravityswitch : MonoBehaviour
+{
+    public Rigidbody2D playerrigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag=="Player"){
+            print("please");
+            playerrigidbody=other.gameObject.GetComponent<Rigidbody2D>();
+            playerrigidbody.gravityScale *= -1;
+            Vector3 newDirection = transform.localScale;
+            newDirection.y *= -1;
+            transform.localScale = newDirection;
+        }
+    }
+    
+}
